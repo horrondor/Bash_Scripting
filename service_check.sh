9f0cf635106f754b1f0b7c68e18ed0e29264ffe3
Raju Bhattarai <rbhattarai525@gmail.com>
2025-04-22 01:25:21 +0545

    file(restart_service):Restarting service feature is added

--- service_check.sh
+++ service_check.sh
@@ -7,5 +7,12 @@ service_name="apache2"
 if systemctl is-active --quiet $service_name; then
     echo "$service_name is running"
 else
-    echo "$service_name is not running"    
-fi    
+    echo  $service_name is not running... restarting it now
+    sudo systemctl restart $service_name
+
+    if systemctl is-active --quiet $service_name; then   
+        echo "$service_name has successfullt restarted."
+    else
+        echo "failed to restart $service_name"    
+    fi  
+fi      
